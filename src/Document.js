b743399f9531f6c28a0762fc0b62f876491f563f
ElitsaDev <94979851+ElitsaDev@users.noreply.github.com>
2023-02-07 22:39:25 +0200

    fetch data and add event onScroll

---
+++ src/Document.js
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Document(props) {
+    let title = props.title;
+    let content = props.content;
+    const [ disable, setDisable] = useState(true);
+    
+    
+
+    const handleScroll = e => {
+        const atBottom = Math.ceil(e.target.scrollTop + e.target.clientHeight) >= e.target.scrollHeight;
+
+        if (atBottom) {
+            setDisable(false);
+        }
+    };
+    return (
+        <>
+        <div className="content" title={title} onScroll={handleScroll}
+            style={
+                {
+                    overflowY: "scroll",
+                    textOverflow: "clip",
+                    height: "400px"
+                }
+            }
+        >{content}</div>
+        <button disabled={disable}
+                    >I Agree</button>
+        </>
+    );
+}
